3378db2eb3bfcc1829ea9774bc39280988a78212
Phuc Huynh <huynhbaophuc179a@gmail.com>
2022-08-07 16:05:38 +0700

    Added Gatsby Tutorial part 4 completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: `Gatsby Blog Site`,
   },
   plugins: [
 
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
